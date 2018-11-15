03edb9a6774e4d01a4ca1bbc78801f4dec654a4e
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add new comments
    Add a member-function which performs multiplication of two float point
    digits.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,20 @@
 #include "calculator.h"
-
+//liquidated personnel - 55110 (+300)
+//tanks - 2227 (+11)
+//armored personnel vehicle - 4748 (+24)
 int Calculator::Add (int a, int b)
 {
     return a + b;
 }
-
+//artillery systems/mlrs - 1340/318 (+17/0)
+//anti-aircraft warfare systems - 168
+//aircraft/helicopters - 253/217 (+1/0)
+//warships - 15
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+//random text random text
+//uav otl - 932 (+7)
+//cruise missiles - 239
+//vehicles & fuel tanks - 3610 (+23)
--- calculator.h
+++ calculator.h
@@ -1,6 +1,11 @@
 class Calculator
 {
     public:
+<<<<<<< HEAD
         static int Add (int, int);
         static int Mul (double, double);
+=======
+        int Add (int, int);
+        int Mul (double, double);
+>>>>>>> e0ea21b (add a multiplication operation)
 };
